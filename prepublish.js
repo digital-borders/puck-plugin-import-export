c3a6641b3d7e10fd3fa8b515fd3820ed59b1f57f
Carmelo Fiorello <carmelofiorello@gmail.com>
2026-03-01 21:08:20 +0100

    chore: add prepublish script to avoid running `npm publish` directly

---
+++ prepublish.js
@@ -0,0 +1,17 @@
+const RELEASE_MODE = !!process.env.RELEASE_MODE;
+
+if (!RELEASE_MODE) {
+  console.log(
+    'YOU MUST NOT RUN THIS COMMAND! USE `npm run release` TO PUBLISH A NEW VERSION',
+  );
+  process.exit(1); //which terminates the publish process
+}
+console.log('PUBLISHING IN RELEASE MODE');
+// Check also that node is on version 16.20.0 using cli
+const { execSync } = require('child_process');
+const nodeVersion = execSync('node --version').toString();
+
+if (!nodeVersion.includes('v16.20.0')) {
+  console.log('YOU MUST USE NODE VERSION 16.20.0');
+  process.exit(1);
+}
