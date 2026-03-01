7c1da2e6e3e8ba509470e9914e23ae231ddae0c9
Carmelo Fiorello <carmelofiorello@gmail.com>
2026-03-01 21:11:38 +0100

    fix: update node version requirement and standardize string quotes in prepublish.js

--- prepublish.js
+++ prepublish.js
@@ -1,18 +1,20 @@
-import { execSync } from 'child_process';
+import { execSync } from "child_process";
 
 const RELEASE_MODE = !!process.env.RELEASE_MODE;
 
 if (!RELEASE_MODE) {
   console.log(
-    'YOU MUST NOT RUN THIS COMMAND! USE `npm run release` TO PUBLISH A NEW VERSION',
+    "YOU MUST NOT RUN THIS COMMAND! USE `npm run release` TO PUBLISH A NEW VERSION",
   );
   process.exit(1); //which terminates the publish process
 }
-console.log('PUBLISHING IN RELEASE MODE');
-// Check also that node is on version 16.20.0 using cli
-const nodeVersion = execSync('node --version').toString();
+console.log("PUBLISHING IN RELEASE MODE");
+// Check also that node is on version 22.18.0 using cli
+const nodeVersion = execSync("node --version").toString();
 
-if (!nodeVersion.includes('v16.20.0')) {
-  console.log('YOU MUST USE NODE VERSION 16.20.0');
+if (!nodeVersion.includes("v22.18.0")) {
+  console.log(
+    "YOU MUST USE NODE VERSION 22.18.0 TO BUILD THE PACKAGE AND PUBLISH NEW VERSION",
+  );
   process.exit(1);
 }
